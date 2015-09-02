bf89358ee563fdf7667d3c30983e886701264b5a
Alejandro Carrilo <j.alec.n@gmail.com>
2015-09-02 23:07:38 +0200

    Added Polygon object

---
+++ libraries/geojson/objects/geometry/Polygon.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace geojson\objects\geometry;
+
+use geojson\objects\Geometry;
+use geojson\traits\GeometricBag;
+
+/**
+ * Class Polygon
+ * @package geojson\objects\geometry
+ */
+class Polygon extends Geometry
+{
+    use GeometricBag {
+       add as addGeometry;
+    }
+
+    const INVALID_ARGUMENT_CODE = 1;
+    const NON_LINEARRING_CODE = 2;
+
+    /**
+     * @param LineString $lineString
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function add(LineString $lineString)
+    {
+        if (!($lineString instanceof LineString)) {
+            throw new \InvalidArgumentException(
+                'Only LineString objects may be added to a MultiLineString',
+                self::INVALID_ARGUMENT_CODE
+            );
+        }
+
+        if (!$lineString->isLinearRing()) {
+            throw new \InvalidArgumentException(
+                'The LineString instance passed is not a LinearRing (4 points, first and last must be equal)',
+                self::NON_LINEARRING_CODE
+            );
+        }
+
+        $this->addGeometry([$lineString]);
+    }
+
+    /**
+     * Returns an array of coordinates
+     *
+     * @return array
+     */
+    public function getCoordinates()
+    {
+        return $this->all();
+    }
+}
---
+++ tests/unit/geojson/objects/geometry/PolygonTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace unit_tests\geojson\objects\geometry;
+
+use geojson\objects\geometry\LineString;
+use geojson\objects\geometry\Point;
+use geojson\objects\geometry\Polygon;
+
+/**
+ * Class PolygonTest
+ * @package unit_tests\geojson\objects\geometry
+ */
+class PolygonTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        /**
+         * We need this in order to bypass PHPUnit's handler and test for the real exception
+         */
+        set_error_handler(
+            function () {
+                return true;
+            }
+        );
+    }
+
+    public function tearDown()
+    {
+        restore_error_handler();
+    }
+
+    public function testLinearRingAddition()
+    {
+        $lineString = new LineString(new Point(1, 2), new Point(2, 3));
+        $lineString->add([new Point(3, 4), new Point(4, 1), new Point(1, 2)]);
+
+        $sut = new Polygon();
+        $sut->add($lineString);
+
+        $expected_coordinates = [[[1, 2], [2, 3], [3, 4], [4, 1], [1, 2]]];
+
+        $this->assertEquals($expected_coordinates, $sut->getCoordinates());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedCode \geojson\objects\geometry\Polygon::INVALID_ARGUMENT_CODE
+     */
+    public function testInvalidLinearRingAdditionArgument()
+    {
+        $sut = new Polygon();
+        $sut->add('test');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedCode \geojson\objects\geometry\Polygon::NON_LINEARRING_CODE
+     */
+    public function testNonLinearRingAddition()
+    {
+        $lineString = new LineString(new Point(1, 2), new Point(2, 3));
+
+        $sut = new Polygon();
+        $sut->add($lineString);
+    }
+}
